fa5ebca224c8feefda050906f7ca07f9afd56e3b
Dae Mon <125165282+XienDaeMon@users.noreply.github.com>
2023-02-18 16:56:46 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,25 +1,8 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let imgr = flaaa.getRandom()
-let text = `*SYARAT & KETENTUAN*
-    
-
-📮 S&K
-1. Jangan diperjual belikan Script ini
-2. Jangan salah gunakan script ini!
-3. Jangan di per jual belikan
-• Jika ada eror di sc hub https://wa.me/6282214729677
-
---------Thanks to ✨---------
-Allah SWT
-My parents
-All Friends
-All Contributors
-All Creator Bot
-Adiwajshing
-Nurutomo
-BochilGaming`
+let text = `Sc Nya Gw Private Karna Gw Recode Sendiri Kalo Mau \nSc ORI Cari Sendiri`
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Script di sini', url: 'https://wa.me/6282214729677'}},
+    {index: 1, urlButton: {displayText: 'ESCE', url: 'https://wa.me/6281269734000'}},
 ]
 let tm = {
 text: text,
